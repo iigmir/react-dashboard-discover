b114174496f165b2e0fb118ab8cfc934f0eeddc7
iigmir <roc120j@gmail.com>
2022-07-02 00:57:23 +0800

    install material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Button from '@mui/material/Button';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button variant="contained">Hello World</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
