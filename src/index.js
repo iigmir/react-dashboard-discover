815b24b06024777d31cfd2ee3a63d1f6983bdc33
iigmir <roc120j@gmail.com>
2022-07-02 02:45:57 +0800

    Fix

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import { Provider } from "react-redux"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-    <App />
-  </Provider>
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>
 </React.StrictMode>);
 
