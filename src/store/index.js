47ff57a02deca713ea8df571e0e9aabf64db1e2d
iigmir <roc120j@gmail.com>
2022-07-02 02:45:15 +0800

    Fxi store

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import ReduxMainStore from "./app";
 
-export const ReduxStoreApp = createSlice({
-    name: "app_store",
-    initialState: {
-        list: [],
-    },
-    reducers: {
-        SET_list: (state, action = { payload: [] }) => {
-            state.list = action.payload;
-        },
-    },
-});
-
-// Redux state and action APIs
-export const { SET_list } = ReduxStoreApp.actions;
-export const ReduxList = (state) => state.list;
-// Reducers
-export default ReduxStoreApp.reducer;
+export default configureStore({
+  reducer: ReduxMainStore
+})
