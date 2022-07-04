a1667650a59752d1968ddf16ceccb1a74f088dce
iigmir <roc120j@gmail.com>
2022-07-04 22:28:10 +0800

    Add bar back

--- src/App/AsideMenu.js
+++ src/App/AsideMenu.js
@@ -8,6 +8,7 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
+import Toolbar from "@mui/material/Toolbar";
 // MUI Icons
 import MailIcon from "@mui/icons-material/Mail";
 import HomeIcon from "@mui/icons-material/Home";
@@ -49,6 +50,8 @@ export default class AsideMenu extends React.Component {
     }
     render() {
         return (<Drawer sx={this.state.sx} variant="permanent" anchor="left">
+            <Toolbar />
+            <Divider />
             <List>
                 { <ListLink item={{ path: "/", text: "Home", icon: <HomeIcon /> }} /> }
             </List>
