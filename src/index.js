7d5d16631a1dacdcea6d455fca7d7a251ea594ac
iigmir <roc120j@gmail.com>
2022-07-04 23:20:03 +0800

    Init

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import { Provider } from "react-redux"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode>
+    {/* Init router */}
     <BrowserRouter>
+        {/* Init redux */}
         <Provider store={store}>
             <Layout />
         </Provider>
