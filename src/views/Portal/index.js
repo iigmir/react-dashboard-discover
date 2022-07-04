7755aecf3cf5db16e37e30bdd6dae2aacbc2128b
iigmir <roc120j@gmail.com>
2022-07-04 22:27:24 +0800

    Add routes

---
+++ src/views/Portal/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Portal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (<div>
+            <h1>Hello World</h1>
+        </div>);
+    }
+}
